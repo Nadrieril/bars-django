b2859b2f9acfb963832245696404da108dafe525
Fatou <teintnoir1994@gmail.com>
2016-01-02 11:12:40 +0100

    Prevent a respo bar to cancel a punishment against him

--- bars_transactions/perms.py
+++ bars_transactions/perms.py
@@ -2,6 +2,8 @@ from datetime import timedelta
 from django.utils import timezone
 from permission.logics import AuthorPermissionLogic
 from bars_core.perms import debug_perm
+from bars_core.perms import BarRolePermissionLogic
+
 
 
 class TransactionAuthorPermissionLogic(AuthorPermissionLogic):
@@ -10,15 +12,16 @@ class TransactionAuthorPermissionLogic(AuthorPermissionLogic):
         if not user.is_authenticated() or not user.is_active:
             return False
 
+        permission = BarRolePermissionLogic().has_perm(user, perm, obj)
         if obj is not None and perm == 'bars_transactions.change_transaction':
             threshold = obj.bar.settings.transaction_cancel_threshold
-            if timezone.now() - obj.timestamp > timedelta(hours=threshold):
+            if timezone.now() - obj.timestamp > timedelta(hours=threshold) and not permission:
                 return False
-            if obj.type == "punish":
-                aop = obj.accountoperation_set.all()[0]
-                if aop.target.owner == user:
-                    return False
-                elif user.is_superuser and aop.target.owner != user:
-                    return True
 
-        return super(TransactionAuthorPermissionLogic, self).has_perm(user, perm, obj)
+            if permission and obj.type == "punish" and obj.accountoperation_set.all()[0].target.owner == user:
+                return False
+
+        if not permission:
+            return super(TransactionAuthorPermissionLogic, self).has_perm(user, perm, obj)
+        else:
+            return True
