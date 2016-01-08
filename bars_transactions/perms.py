d70e79bb6d7954f9f0c1846484ea115a98952912
Fatou <teintnoir1994@gmail.com>
2016-01-08 23:24:47 +0100

    Tests for the PunishTransaction and comments in Transactions.perms.py

--- bars_transactions/perms.py
+++ bars_transactions/perms.py
@@ -8,6 +8,11 @@ from bars_core.perms import BarRolePermissionLogic
 
 class TransactionAuthorPermissionLogic(AuthorPermissionLogic):
     @debug_perm("Logic (transaction)")
+
+        #Here we define the permissions that users need to cancel, create some transaction
+        #AuthorPermissionLogic is to be sure that you can cancel the operation you are responsible for
+        #We use BarRolePermissionLogic to enable or not an action according to the roles of the user
+
     def has_perm(self, user, perm, obj=None):
         if not user.is_authenticated() or not user.is_active:
             return False
@@ -15,10 +20,10 @@ class TransactionAuthorPermissionLogic(AuthorPermissionLogic):
         permission = BarRolePermissionLogic().has_perm(user, perm, obj)
         if obj is not None and perm == 'bars_transactions.change_transaction':
             threshold = obj.bar.settings.transaction_cancel_threshold
-            if timezone.now() - obj.timestamp > timedelta(hours=threshold) and not permission:
+            if timezone.now() - obj.timestamp > timedelta(hours=threshold) and not permission: #Basic user that cannot cancel operation
                 return False
 
-            if permission and obj.type == "punish" and obj.accountoperation_set.all()[0].target.owner == user:
+            if permission and obj.type == "punish" and obj.accountoperation_set.all()[0].target.owner == user: #Prevent the staff to cancel punishment against him
                 return False
 
         if not permission:
