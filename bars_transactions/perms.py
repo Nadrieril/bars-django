251269f579bf2e686071025f7f905d85b10b20fb
Fatou <teintnoir1994@gmail.com>
2016-01-10 21:18:49 +0100

    TestPunishTransaction

--- bars_transactions/perms.py
+++ bars_transactions/perms.py
@@ -1,32 +1,27 @@
 from datetime import timedelta
 from django.utils import timezone
 from permission.logics import AuthorPermissionLogic
-from bars_core.perms import debug_perm
-from bars_core.perms import BarRolePermissionLogic
-
-
+from bars_core.perms import debug_perm, BarRolePermissionLogic
 
 class TransactionAuthorPermissionLogic(AuthorPermissionLogic):
     @debug_perm("Logic (transaction)")
-
-        #Here we define the permissions that users need to cancel, create some transaction
-        #AuthorPermissionLogic is to be sure that you can cancel the operation you are responsible for
-        #We use BarRolePermissionLogic to enable or not an action according to the roles of the user
-
     def has_perm(self, user, perm, obj=None):
         if not user.is_authenticated() or not user.is_active:
             return False
-
-        permission = BarRolePermissionLogic().has_perm(user, perm, obj)
+    
+        bar_role_perm = BarRolePermissionLogic().has_perm(user, perm, obj)
+        
         if obj is not None and perm == 'bars_transactions.change_transaction':
+            # Nobody can cancel a punishment that he received
+            if obj.type == "punish" and obj.accountoperation_set.all()[0].target.owner == user:
+                return False
+            # We check general bar-related permissions
+            if bar_role_perm:
+                return True
+            # Otherwise, general rule for transaction update
             threshold = obj.bar.settings.transaction_cancel_threshold
-            if timezone.now() - obj.timestamp > timedelta(hours=threshold) and not permission: #Basic user that cannot cancel operation
+            if timezone.now() - obj.timestamp > timedelta(hours=threshold):
                 return False
 
-            if permission and obj.type == "punish" and obj.accountoperation_set.all()[0].target.owner == user: #Prevent the staff to cancel punishment against him
-                return False
-
-        if not permission:
-            return super(TransactionAuthorPermissionLogic, self).has_perm(user, perm, obj)
-        else:
-            return True
+# For other cases
+                    return bar_role_perm or super(TransactionAuthorPermissionLogic, self).has_perm(user, perm, obj)
--- bars_transactions/tests/test_transactions.py
+++ bars_transactions/tests/test_transactions.py
@@ -20,28 +20,28 @@ def reload(obj):
 
 class TransactionTests(APITestCase):
     @classmethod
-
+    
     #Set up data for TransactionTests : implementation of users, items, transactions
     def setUpTestData(self):
         super(TransactionTests, self).setUpTestData()
         self.bar, _ = Bar.objects.get_or_create(id='barjone')
         self.wrong_bar, _ = Bar.objects.get_or_create(id='barrouje')
-
+        
         self.user, _ = User.objects.get_or_create(username='user')
         self.user.role_set.all().delete()
         self.user = reload(self.user)
         self.account, _ = Account.objects.get_or_create(bar=self.bar, owner=self.user)
         self.account.money = 100
         self.account.save()
-
+        
         self.wrong_user, _ = User.objects.get_or_create(username='wrong_user')
         self.wrong_account, _ = Account.objects.get_or_create(bar=self.wrong_bar, owner=self.wrong_user)
-
+        
         #we define a member of the staff in the bar
         self.staff_user, _ = User.objects.get_or_create(username='staff_user')
         self.staff_account, _ = Account.objects.get_or_create(bar=self.bar, owner=self.staff_user)
         self.staff_role, _ = Role.objects.get_or_create(name='staff', bar=self.bar, user=self.staff_user)
-
+        
         self.sellitem, _ = SellItem.objects.get_or_create(bar=self.bar, name="Chocolat", tax=0.2)
         self.itemdetails, _ = ItemDetails.objects.get_or_create(name="Chocolat")
         self.buyitem, _ = BuyItem.objects.get_or_create(details=self.itemdetails, itemqty=2.5)
@@ -49,52 +49,51 @@ class TransactionTests(APITestCase):
         self.stockitem.unit_factor = 5
         self.stockitem.qty = 5
         self.stockitem.save()
-
+        
         self.transaction = Transaction.objects.create(bar=self.bar, author=self.user)
         self.transaction_url = '/transaction/%d/' % self.transaction.id
-
-
+    
+    
     def setUp(self):
         self.transaction.canceled = False
         self.transaction.save()
         Role.objects.get_or_create(user=self.user, bar=self.bar, name='customer')
         self.user = reload(self.user)
         self.staff_user = reload(self.staff_user)
-
+    
     def test_cancel_transaction(self):
         self.client.force_authenticate(user=self.user)
         response = self.client.put(self.transaction_url + 'cancel/', {})
         self.assertEqual(response.status_code, 200)
         self.assertTrue(reload(self.transaction).canceled)
-
+    
     def test_restore_transaction(self):
         self.transaction.canceled = True
         self.transaction.save()
         self.client.force_authenticate(user=self.user)
-
+        
         response = self.client.put(self.transaction_url + 'restore/', {})
         self.assertEqual(response.status_code, 200)
         self.assertFalse(reload(self.transaction).canceled)
-
+    
     def test_cancel_transaction_wrong_user(self):
         self.client.force_authenticate(user=self.wrong_user)
-
+        
         response = self.client.put(self.transaction_url + 'cancel/', {})
         self.assertEqual(response.status_code, 403)
         self.assertFalse(reload(self.transaction).canceled)
-
+    
     def test_cancel_transaction_staff(self):
         self.client.force_authenticate(user=self.staff_user)
-
+        
         response = self.client.put(self.transaction_url + 'cancel/', {})
         self.assertEqual(response.status_code, 200)
         self.assertTrue(reload(self.transaction).canceled)
-
+    
     def test_cancel_transaction_staff_wrong_bar(self):
         self.client.force_authenticate(user=self.staff_user)
         transaction = Transaction.objects.create(bar=self.wrong_bar, author=self.wrong_user)
-
+        
         response = self.client.put('/transaction/%d/cancel/' % transaction.id, {})
         self.assertEqual(response.status_code, 403)
         self.assertFalse(reload(transaction).canceled)
-
